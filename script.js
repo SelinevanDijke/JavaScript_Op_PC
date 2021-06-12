5b273a32fd2590fd137ffa4f1ed355b133eb3f14
Seline <74591087+SelinevanDijke@users.noreply.github.com>
2021-06-12 13:35:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Deel 1
+console.log("Hallo Winc Academy studenten");
+
+// Deel 2
+// Dit is een grote som, deze comment zie je niet omdat het uitstaat.
+let multiplication = 1230941 * 1823794;
+console.log("Multiplication", multiplication);
+
+let division = 637263 / 54;
+console.log("Division", division);
